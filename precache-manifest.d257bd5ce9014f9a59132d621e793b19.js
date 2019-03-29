52872d0cfd7d2c74bb67639360383b69879c3835
黃哲芬 <annie101101046@gmail.com>
2019-03-29 11:25:22 +0800

    Updates

---
+++ precache-manifest.d257bd5ce9014f9a59132d621e793b19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7dcedf3deb26e2d6e7f9",
+    "url": "/React-game/static/css/main.b533c0f2.chunk.css"
+  },
+  {
+    "revision": "7dcedf3deb26e2d6e7f9",
+    "url": "/React-game/static/js/main.9b1387a5.chunk.js"
+  },
+  {
+    "revision": "cb2ffbd55fa9d9d408a5",
+    "url": "/React-game/static/js/runtime~main.e3737006.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/React-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bb7281bbb896d91a14dfcbc44454ccf6",
+    "url": "/React-game/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/React-game/precache-manifest.d257bd5ce9014f9a59132d621e793b19.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/React-game/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
